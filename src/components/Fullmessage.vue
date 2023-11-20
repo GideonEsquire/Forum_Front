d98cd4e9c6e74115b95aa99c9781cddd73fcffc0
Gideon <GideonEsquire@gmail.com>
2023-11-20 18:43:24 +0000

    Added some buttons

--- src/components/Fullmessage.vue
+++ src/components/Fullmessage.vue
@@ -3,12 +3,36 @@
 
 <template>
   <div>
+
+    <!-- TODO: Add colored line on left to indicate level of nesting -->
+    <!-- TODO: Have a hidden menu under card to hide this message or all messages from this author -->
     <v-card class="pa-5 mb-4">
       <p>
       <slot></slot>
       </p>
-      <v-btn><v-icon start icon="mdi-check"></v-icon>cool</v-btn>
-      <v-btn color="error"><v-icon start icon="mdi-close"></v-icon>not cool</v-btn>
+
+      <!-- TODO: When clicked, popup with author Name and button to set filter of the author. -->
+      <v-avatar>
+        <v-img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+            ></v-img>
+      </v-avatar>
+
+      <!-- TODO: Toggle star.  -->
+      <v-btn>
+        <v-tooltip open-delay="500" activator="parent" location="top" >Save this message</v-tooltip>
+        <v-icon icon="mdi-star"></v-icon></v-btn>
+
+      <!-- TODO: Toggle view properties -->
+      <v-btn>
+        <v-tooltip open-delay="500" activator="parent" location="top" >View properties</v-tooltip>
+        <v-icon icon="mdi-hamburger"></v-icon>
+      </v-btn>
+      <v-btn>
+        <v-tooltip open-delay="500" activator="parent" location="top" >Reply to this message</v-tooltip>
+        <v-icon icon="mdi-comment"></v-icon>
+      </v-btn>
     </v-card>
   </div>
 </template>
--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import FullMessage from "@/components/Fullmessage.vue"
 
-const messagesList = [" adipisicing elit. Cum voluptatibus voluptates numquam, asperiores, vero est itaque odio excepturi quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In facilis quae, eligendi dicta praesentium obcaecati iusto rerum non magnam similique placeat eaque deserunt at magni eius, iste neque! Quasi, molestiae?"]
+const messagesList = [
+"adipisicing elit. Cum voluptatibus voluptates numquam, asperiores, vero est itaque odio excepturi quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!",
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit. In facilis quae, eligendi dicta praesentium obcaecati iusto rerum non magnam similique placeat eaque deserunt at magni eius, iste neque! Quasi, molestiae?",
+"elit. Cum voluptatibus voluptates numquam, asperiores, vero est itaque odio excepturi quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!",
+"L, consectetur adipisicing elit. In facilis quae, eligendi dicta praesentium obcaecati iusto rerum non magnam similique placeat eaque deserunt at magni eius, iste neque! Quasi, molestiae?",
+"L,similique placeat eaque deserunt at magni eius, iste neque! Quasi, molestiae?",
+"elit. Cum voluptatibus voluptates numquam, asperiores, vero est itaque odio excepturi quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!",
+" quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!",
+"asperiores, vero est itaque odio excepturi quam ab repudiandae veritatis iure nulla, quas eius! Tenetur sed culpa cumque!",
+]
 </script>
 
 <template>
   <div>
     <v-main>
       <v-container>
-        <h1>Home</h1>
+        <h1>Stream</h1>
         <FullMessage v-for="item in messagesList">
         {{item}}
         </FullMessage>
